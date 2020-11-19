ae06471132091e02afcf869747f8edcb9112bc87
peteg96 <60364146+peteg96@users.noreply.github.com>
2020-11-18 21:01:06 -0800

    light game update

--- Project_Bokkusu/Assets/Script/Candle.cs
+++ Project_Bokkusu/Assets/Script/Candle.cs
@@ -9,7 +9,7 @@ public class Candle : MonoBehaviour
     public GameObject fire;
    
     bool state = true;
-
+    
     private void Start()
     {
        
@@ -19,20 +19,20 @@ public class Candle : MonoBehaviour
         
         if (col.gameObject.tag == "Player" && Input.GetKeyUp(KeyCode.E))
         {
-           
+            
             if (state == true) state = false;
             else state = true;
             //switch the state 
         }
-        light.SetActive(state);// turn on or off the light
-        fire.SetActive(state);
+        
         
     }
-
-    private void OnTriggerExit(Collider other)
+    private void Update()
     {
-       
+        light.SetActive(state);// turn on or off the light
+        fire.SetActive(state);
     }
+   
 
 
 }
