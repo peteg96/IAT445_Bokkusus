0090d0bb0f3eaa6b23fc3ebbe709efa6e164b787
1105959326 <69542207+1105959326@users.noreply.github.com>
2020-12-03 20:05:12 -0800

    animatior

---
+++ Project_Bokkusu/Assets/Script/dooropen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dooropen : MonoBehaviour
+{
+    public Animator doorL, doorR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //doorL = GetComponent<Animator>();
+        //doorR = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Player")
+        {
+            //print(doorLeft.SetInteger("state"));
+            doorL.SetInteger("state", 1);
+            doorR.SetInteger("state", 1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            doorL.SetInteger("state", -1);
+            doorR.SetInteger("state", -1);
+        }
+    }
+}
