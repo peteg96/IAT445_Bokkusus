1289309015b4707d3926d7d34a6409b1c4c67622
peteg96 <60364146+peteg96@users.noreply.github.com>
2020-12-08 23:42:08 -0800

    update transparent code

---
+++ Project_Bokkusu/Assets/Pete/Transparency.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour
+{
+    public GameObject girl;
+    float r, g, b, t;
+
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = 62;
+        g = 66;
+        b = 82;
+        t = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            var material = girl.transform.GetChild(i);
+            material.GetComponent<Renderer>().enabled = false;
+        }
+        x = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x-=Time.deltaTime;
+        print(x);
+        if (x < 0)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var material = girl.transform.GetChild(i);
+                material.GetComponent<Renderer>().enabled = true;
+            }
+            t += 0.0003f;
+            if (t >= 0.5f)
+            {
+                t = 0.5f;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                var material = girl.transform.GetChild(i);
+                ChangeAlpha(material.GetComponent<Renderer>().material, t);
+            }
+        }
+        //ChangeAlpha(girl.GetComponent<Renderer>().material, t);
+        
+        
+    }
+    void ChangeAlpha(Material mat, float alphaVal)
+    {
+        Color oldColor = mat.color;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+        mat.SetColor("_Color", newColor);
+
+    }
+}
