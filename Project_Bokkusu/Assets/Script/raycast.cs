3888df1a5af88ddab0ea39955a7e0dada61c5499
1105959326 <69542207+1105959326@users.noreply.github.com>
2020-12-10 05:44:05 -0800

    UIupdate

--- Project_Bokkusu/Assets/Script/raycast.cs
+++ Project_Bokkusu/Assets/Script/raycast.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class raycast : MonoBehaviour
 {
-    int layer = 1 << 8; //ray will only interate with items in layer 8
-    public GameObject spirit, UI;
-    float timer = 8.0f;
+    int layer = 1 << 8, num = 0; //ray will only interate with items in layer 8
+    public GameObject spirit, UI, frontDoor, mainDoor, leftRoom, rightRoom;
+    float timer;
+    bool start;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,23 +17,50 @@ public class raycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //print(timer);
         Vector3 forward = transform.TransformDirection(Vector3.forward * 2); //set the direction of the ray
         if (Physics.Raycast(transform.position, forward, 50, layer) &&
             spirit.GetComponent<Animator>().GetInteger("state") == 0)
         {
-            timer -= Time.deltaTime;
+            timer = 11f;
             UI.SetActive(true);
-            UI.GetComponent<Type>().fullText = "Sprite: \n Knight release me.";
+            UI.GetComponent<Type>().fullText = "Sprite:\nMy Honorable lord, I am an innocent soul imprisoned here for so long." +
+                " \nHelp me, I shall lead the way.";
+        }
+        else if (mainDoor.GetComponent<roomCheck>().inRoomCheck && num == 0)
+        {
+            num = 1;
+            timer = 3f;
+            UI.SetActive(true);
+            UI.GetComponent<Type>().fullText = "Sprite:\nI'm glad you've come.";
+        }
+        else if (leftRoom.GetComponent<roomCheck>().inRoomCheck || rightRoom.GetComponent<roomCheck>().inRoomCheck)
+        {
+            
+            if (num == 1)
+            {
+                num = 2;
+                timer = 7f;
+                UI.SetActive(true);
+                UI.GetComponent<Type>().fullText = "Sprite:\nThat is unfortunate…\nI assure you the door will be open if you complete the puzzle.";
+            }
         }
 
 
-
-        //if (Physics.Raycast(transform.position, forward, 50, layer) && spirit.GetComponent<Transform>().position == new Vector3((float)19.5, (float)1.8682, (float)13.44))spirit.GetComponent<Animator>().SetInteger("state", 2); // check is object reach the position, if yes, change state
-
-        if (timer < 0)
+        if(timer > 0) timer -= Time.deltaTime;
+        else if (timer < 0)
         {
-            UI.SetActive(false);
+            UI.GetComponent<Type>().fullText = "Sprite:";
+            if (UI.GetComponent<Type>().textShow == "Sprite:") UI.SetActive(false);
+            if (spirit.GetComponent<Animator>().GetInteger("state") == 0)
             spirit.GetComponent<Animator>().SetInteger("state", 1); //check animation state and is ray hit object, if yes, change state
         }
+
+
+        if (frontDoor.GetComponent<dooropen>().open && spirit.GetComponent<Animator>().GetInteger("state") == 1)
+        {
+            spirit.GetComponent<Animator>().SetInteger("state", 3);
+        }
+
     }
 }
