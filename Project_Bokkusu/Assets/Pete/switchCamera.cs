7e99e699eb64d009745680f7a8828b1185a068a3
1105959326 <69542207+1105959326@users.noreply.github.com>
2020-12-09 19:04:16 -0800

    senceU

--- Project_Bokkusu/Assets/Pete/switchCamera.cs
+++ Project_Bokkusu/Assets/Pete/switchCamera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class switchCamera : MonoBehaviour
 {
-    public GameObject Player, state_left, state_right;
+    public GameObject Player, state_left, state_right, box;
     public GameObject Camera;
     
     bool reveal = false;
-    float time_left = 5.0f, time_right = 5.0f;
+    float time_left = 8.0f, time_right = 8.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +20,33 @@ public class switchCamera : MonoBehaviour
     void Update()
     {
 
-        if (state_left.GetComponent<state_detector>().num == 6 && time_left > 0)
+        if (state_left.GetComponent<state_detector>().num == 6 && time_left > 5) time_left -= Time.deltaTime;
+        else if (state_left.GetComponent<state_detector>().num == 6 && time_left > 0)
         {
 
             //reveal the result and the camera switch 
             Player.GetComponent<Camera>().gameObject.SetActive(false);
             Camera.GetComponent<Camera>().gameObject.SetActive(true);
+            
             time_left -= Time.deltaTime;
+            if (time_left < 2f)box.GetComponent<Animator>().SetBool("shake", true);
+            
         }
+        else if (state_right.GetComponent<game_light>().state == true && time_right > 5) time_right -= Time.deltaTime;
         else if(state_right.GetComponent<game_light>().state == true && time_right > 0)
         {
             Player.GetComponent<Camera>().gameObject.SetActive(false);
             Camera.GetComponent<Camera>().gameObject.SetActive(true);
             Camera.GetComponent<Animator>().SetBool("state", true);
             time_right -= Time.deltaTime;
+            if (time_right < 2f) box.GetComponent<Animator>().SetBool("shake", true);
         }
         else
         {
             //switch back to player perspective
             Camera.GetComponent<Camera>().gameObject.SetActive(false);
             Player.GetComponent<Camera>().gameObject.SetActive(true);
-
+            box.GetComponent<Animator>().SetBool("shake", false);
         }
     }
 }
---
+++ Project_Bokkusu/Assets/Script/Transparency.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour
+{
+    public GameObject girl, box;
+    float  t;
+    bool state = false;
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            var material = girl.transform.GetChild(i);
+            material.GetComponent<Renderer>().enabled = false;
+        }
+        x = 1.5f;
+        girl.GetComponent<AudioSource>().Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state)
+        {
+            box.GetComponent<Animator>().SetBool("open", true);
+            x -= Time.deltaTime;
+            
+            if (x < 0)
+            {
+                if (girl.GetComponent<AudioSource>().isPlaying == false) girl.GetComponent<AudioSource>().Play();
+                for (int i = 0; i < 4; i++)
+                {
+                    var material = girl.transform.GetChild(i);
+                    material.GetComponent<Renderer>().enabled = true;
+                }
+                t += 0.0005f;
+
+                if (t >= 0.2f)//fade music
+                {
+                    //print(girl.GetComponent<AudioSource>().volume);
+                    girl.GetComponent<AudioSource>().volume -= 0.001f;
+                }
+
+                if (t >= 0.5f)
+                {
+                    t = 0.5f;
+                    girl.GetComponent<AudioSource>().Stop();
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    var material = girl.transform.GetChild(i);
+                    ChangeAlpha(material.GetComponent<Renderer>().material, t);
+                }
+            }
+        }
+        //ChangeAlpha(girl.GetComponent<Renderer>().material, t);
+        
+        
+    }
+    void ChangeAlpha(Material mat, float alphaVal)
+    {
+        Color oldColor = mat.color;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+        mat.SetColor("_Color", newColor);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") state = true;
+    }
+}
