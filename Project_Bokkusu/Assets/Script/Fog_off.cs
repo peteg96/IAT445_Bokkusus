15b444f87a5206f5c2b4a19f1eb5c1f4fd8f708a
peteg96 <60364146+peteg96@users.noreply.github.com>
2020-12-09 20:13:43 -0800

    fog update

---
+++ Project_Bokkusu/Assets/Script/Fog_off.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog_off : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player" )
+        {
+            RenderSettings.fog = false;
+            print("++++");
+        }
+
+
+    }
+}
--- Project_Bokkusu/Assets/Script/item.cs
+++ Project_Bokkusu/Assets/Script/item.cs
@@ -47,7 +47,7 @@ public class item : MonoBehaviour
         if (other.tag == this.tag && Input.GetMouseButton(0) == false && triggered == false)
         {
             this.transform.position = new Vector3(other.transform.position.x, 2, other.transform.position.z);
-            this.transform.rotation = new Quaternion(0, 90, 0, 0);
+            this.transform.rotation = new Quaternion(0, 0, 0, 0);
             this.GetComponent<Rigidbody>().useGravity = true;
             this.GetComponent<BoxCollider>().enabled = true;
             triggered = true;
