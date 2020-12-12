77a5e9b81311fc6d13c2d803b566ee109825d7ff
QatarWen <71157039+QatarWen@users.noreply.github.com>
2020-12-11 17:53:35 -0800

    m

---
+++ Project_Bokkusu/Assets/Andrew/TextMesh Pro/changescene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changescene : MonoBehaviour
+{
+    public string scene_name;
+    public void Scene1changscene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+    
+}
